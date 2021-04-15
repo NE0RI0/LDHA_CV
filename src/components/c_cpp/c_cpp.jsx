2c855e70d477f0971970a984b03954926078e4a9
Luis Daniel Herrera Aguirre <herreraluyis08@outlook.com>
2021-04-15 13:57:53 -0600

    Added pictures, and added base code for web and hardware pages

--- src/components/c_cpp/c_cpp.jsx
+++ src/components/c_cpp/c_cpp.jsx
@@ -1,15 +1,99 @@
-import React from 'react';
+import React from "react";
+import { Card, CardColumns } from "react-bootstrap";
 
+// import "./styles/python_info.css";
 
-
-function CPP_info(){
-    return(
-        <div className='home_div'>
-            <div className='intro_div'>
-                <h1>Hello CPP!</h1>
-            </div>
-        </div>
-    )
+function CPP_info() {
+  const Projects = [
+    {
+      img:
+        "https://github.com/NE0RI0/LDHA_CV/blob/main/src/global/resources/img/projects/DroneTerminal.png?raw=true",
+      title: "Ultrasonic Control",
+      about: "",
+      technologies: "Python, OpenV, Sockets",
+      status: "",
+    },
+    {
+      img:
+        "https://github.com/NE0RI0/LDHA_CV/blob/main/src/global/resources/img/projects/database.png?raw=true",
+      title: "Custom Railed Drone",
+      about: "",
+      technologies: "",
+      status: "",
+    },
+    {
+      img: "",
+      title: "Custom Drone",
+      about: "",
+      technologies: "",
+      status: "",
+    },
+    {
+      img: "",
+      title: "Vision system to get measures",
+      about: "",
+      technologies: "",
+      status: "",
+    },
+    {
+      img: "",
+      title: "",
+      about: "",
+      technologies: "",
+      status: "",
+    },
+    {
+      img: "",
+      title: "",
+      about: "",
+      technologies: "",
+      status: "",
+    },
+    {
+      img: "",
+      title: "",
+      about: "",
+      technologies: "",
+      status: "",
+    },
+    // {
+    //   img: "",
+    //   title: "",
+    //   about: "",
+    //   technologies: "",
+    //   status: "",
+    // },
+  ];
+  return (
+    <div className="python-div">
+      <div className="">
+        <h1>welcomes</h1>
+        <CardColumns>
+          {Projects.map((project) => (
+            <Card className="bg-dark text-white">
+              <Card.Img variant="top" src={project.img} />
+              <Card.Body className="project-card">
+                <Card.Title>{project.title}</Card.Title>
+                <Card.Text>
+                  This card has supporting text below as a natural lead-in to
+                  additional content.{" "}
+                </Card.Text>
+                <div className="text-right blockquote-footer">
+                  <small className="text-muted">
+                    Someone famous in{" "}
+                    <cite title="Source Title">Source Title</cite>
+                  </small>
+                </div>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+          ))}
+        </CardColumns>
+      </div>
+    </div>
+  );
 }
 
 export default CPP_info;
--- src/components/reactjs/react_info.jsx
+++ src/components/reactjs/react_info.jsx
@@ -1,15 +1,71 @@
-import React from 'react';
+import React from "react";
+import { Card, CardColumns } from "react-bootstrap";
 
+// import "./styles/python_info.css";
 
-
-function React_info(){
-    return(
-        <div className='home_div'>
-            <div className='intro_div'>
-                <h1>Hello React!</h1>
-            </div>
-        </div>
-    )
+function React_info() {
+  const Projects = [
+    {
+      img:
+        "https://github.com/NE0RI0/LDHA_CV/blob/main/src/global/resources/img/projects/DroneTerminal.png?raw=true",
+      title: "Opticas 20-20",
+      about: "",
+      technologies: "Python, OpenV, Sockets",
+      status: "",
+    },
+    {
+      img:
+        "https://github.com/NE0RI0/LDHA_CV/blob/main/src/global/resources/img/projects/database.png?raw=true",
+      title: "Drone stadistics API",
+      about: "",
+      technologies: "",
+      status: "",
+    },
+    {
+      img: "",
+      title: "",
+      about: "",
+      technologies: "",
+      status: "",
+    },
+    // {
+    //   img: "",
+    //   title: "",
+    //   about: "",
+    //   technologies: "",
+    //   status: "",
+    // },
+  ];
+  return (
+    <div className="python-div">
+      <div className="">
+        <h1>welcomes</h1>
+        <CardColumns>
+          {Projects.map((project) => (
+            <Card className="bg-dark text-white">
+              <Card.Img variant="top" src={project.img} />
+              <Card.Body className="project-card">
+                <Card.Title>{project.title}</Card.Title>
+                <Card.Text>
+                  This card has supporting text below as a natural lead-in to
+                  additional content.{" "}
+                </Card.Text>
+                <div className="text-right blockquote-footer">
+                  <small className="text-muted">
+                    Someone famous in{" "}
+                    <cite title="Source Title">Source Title</cite>
+                  </small>
+                </div>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+          ))}
+        </CardColumns>
+      </div>
+    </div>
+  );
 }
 
 export default React_info;
