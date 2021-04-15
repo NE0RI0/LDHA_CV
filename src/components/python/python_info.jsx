91801d458b79740efb3647670ad731c511dae438
Luis Daniel Herrera Aguirre <herreraluyis08@outlook.com>
2021-04-14 18:36:32 -0600

    New PC

--- src/components/python/python_info.jsx
+++ src/components/python/python_info.jsx
@@ -3,56 +3,83 @@ import { Card, CardColumns } from 'react-bootstrap';
 import ProjectCard from '../../global/components/project_card/project_card';
 import './styles/python_info.css'
 
-
-
-
 function Python_info(){
-
     const Projects = [
         {
             'img':'',
-            'name':'',
+            'title':'Drone Terminal',
             'about':'',
             'technologies':'',
             'status':'',
         },
         {
             'img':'',
-            'name':'',
+            'title':'Database Inspector',
             'about':'',
             'technologies':'',
             'status':'',
-        },
-        
+        },             
+        {
+            'img':'',
+            'title':'Integrated Writing Environment',
+            'about':'',
+            'technologies':'',
+            'status':'',
+        },             
+        {
+            'img':'',
+            'title':'Frame Extractor',
+            'about':'',
+            'technologies':'',
+            'status':'',
+        },             
+        {
+            'img':'',
+            'title':'YouTube Video Analizer',
+            'about':'',
+            'technologies':'',
+            'status':'',
+        },             
+        {
+            'img':'',
+            'title':'',
+            'about':'',
+            'technologies':'',
+            'status':'',
+        },             
+        {
+            'img':'',
+            'title':'',
+            'about':'',
+            'technologies':'',
+            'status':'',
+        },             
     ]
-
-    
-
     return(
         <div className='python-div'>
             <div className=''>
                 <h1>welcomes</h1>
                 <CardColumns>
-                    
-                    
-                    <Card>
-                        <Card.Img variant="top" src="holder.js/100px160" />
-                        <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This card has supporting text below as a natural lead-in to additional
-                            content.{' '}
-                        </Card.Text>
-                        <div className='text-right blockquote-footer' >
-                        <small className="text-muted">
-                            Someone famous in <cite title="Source Title">Source Title</cite>
-                            </small>
-                        </div>
-                        </Card.Body>
-                        <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card>
+                    {Projects.map(project =>(
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                            <Card.Title>{project.title}</Card.Title>
+                            <Card.Text>
+                                This card has supporting text below as a natural lead-in to additional
+                                content.{' '}
+                            </Card.Text>
+                            <div className='text-right blockquote-footer' >
+                            <small className="text-muted">
+                                Someone famous in <cite title="Source Title">Source Title</cite>
+                                </small>
+                            </div>
+                            </Card.Body>
+                            <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                            </Card.Footer>
+                        </Card>
+                    ))}
 
                     
                     
